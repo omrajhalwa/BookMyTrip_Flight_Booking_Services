76e2580d0ef5c95cf34329391852971b41acabea
omrajhalwa <omrajhalwa@gmail.com>
2024-09-04 13:24:45 +0530

    FEAT: Added booking logic with transactions

--- src/repositories/booking-repository.js
+++ src/repositories/booking-repository.js
@@ -7,6 +7,14 @@ class BookingRepository extends CrudRepository{
     constructor() {
         super(Booking);
     }
+
+    async createBooking(data , transaction) {
+        
+        const response = await Booking.create(data, {transaction:transaction});
+
+        return response;
+    }
+
 }
 
 module.exports = BookingRepository;
