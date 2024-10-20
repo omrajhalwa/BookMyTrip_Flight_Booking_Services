6e2ac7e54e454a669d6c2f6a8384623b342d4f27
omrajhalwa <omrajhalwa@gmail.com>
2024-10-21 00:26:42 +0530

    Added database config file

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -5,7 +5,18 @@ dotenv.config({
     path:".env"
 });
 
-// console.log(process.env.PORT);
+
+const config = require('./config.json');
+
+// Replace placeholders with environment variable values
+const env = process.env.NODE_ENV || 'development';
+config[env].username = process.env.DB_USERNAME || config[env].username;
+config[env].password = process.env.DB_PASSWORD || config[env].password;
+config[env].database = process.env.DB_NAME || config[env].database;
+config[env].host = process.env.DB_HOST || config[env].host;
+
+module.exports = config[env];
+
 module.exports = {
     PORT:process.env.PORT,
     FLIGHT_SERVICE:process.env.FLIGHT_SERVICE
